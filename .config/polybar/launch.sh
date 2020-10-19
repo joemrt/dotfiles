46a7366338367f5e2a632ebc37092730e7cb2439
Joerg Martin <goergi@gmx.net>
2020-10-19 20:14:42 +0200

    bspwm added

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -8,6 +8,14 @@ killall -q polybar
 # Launch bar1 and bar2
 echo "---" | tee -a /tmp/polybar_cusombar.log 
 polybar custombar >>/tmp/polybar_custombar.log 2>&1 & disown
-# polybar bar2 >>/tmp/polybar2.log 2>&1 & disown
+
+ 
+HDMI1_status=$(xrandr -q | awk '/^HDMI1/{print $2}')
+if [ $HDMI1_status == 'connected' ]; then
+		echo "---multiheadbar---" | tee -a /tmp/polybar_cusombar.log 
+		polybar multiheadbar 
+fi
+
+
 
 echo "Bars launched..."
