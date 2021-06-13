1dee77b4d4d555de94976a31d0c1531a230c7cbc
Joerg Martin <goergi@gmx.net>
2021-06-13 11:39:33 +0200

    minimal awesome config

---
+++ .config/awesome/do-at-launch.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# notifications
+dunst &
+# transparency
+picom -b
+
+
+
+# turn of DPMS
+xset s off -dpms
+
+
+
+
+# automount devices
+udiskie --no-notify &
+
+# start DE own services
+systemctl --user start check-battery.timer
+
+# polkit agent for authentification
+/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &>/dev/null &
