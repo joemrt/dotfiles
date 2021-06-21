79559af239d985ec23c4e1a87cf39d1fe48afcf7
Joerg Martin <goergi@gmx.net>
2021-06-21 19:26:38 +0200

    multihead support for awesome wm

--- .config/awesome/do-at-launch.sh
+++ .config/awesome/do-at-launch.sh
@@ -21,3 +21,12 @@ nm-applet &
 # polkit agent for authentification
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &>/dev/null &
 
+# multihead
+HDMI1_status=$(xrandr -q | awk '/^HDMI1/{print $2}')
+
+if [[ $HDMI1_status = 'connected' ]]; then
+	xrandr --output eDP1 --auto --output HDMI1 --auto --right-of eDP1 
+	~/.fehbg
+else
+	xrandr --output eDP1 --auto  --output HDMI1 --off
+fi
