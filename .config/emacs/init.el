d8e23cf364f1f29b1f4f1c4e89b923d298df792f
Joerg Martin <goergi@gmx.net>
2021-07-06 23:09:07 +0200

    init.el updated

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -7,7 +7,7 @@
    ["black" "#d55e00" "#009e73" "#f8ec59" "#0072b2" "#cc79a7" "#56b4e9" "white"])
  '(custom-enabled-themes '(deeper-blue))
  '(package-selected-packages
-   '(linum-relative auctex evil-commentary evil-surround use-package key-chord evil lua-mode ##)))
+   '(org-bullets company-org-block company ivy vterm evil-leader evil-collection magit linum-relative auctex evil-commentary evil-surround use-package key-chord evil lua-mode ##)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -22,10 +22,30 @@
 ;;(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 (package-initialize)
 
+(use-package evil
+  :ensure t
+  :init
+  (setq evil-want-integration t) ;; This is optional since it's already set to t by default.
+  (setq evil-want-keybinding nil)
+  :config
+  (evil-mode 1))
+
+(use-package evil-collection
+  :after evil
+  :ensure t
+  :config
+  (evil-collection-init))
 
 ;; emulate vim behavior
 ;; evil mode
 (setq evil-want-C-u-scroll t)
+(define-key evil-normal-state-map (kbd "C-u") 'evil-scroll-up)
+(define-key evil-visual-state-map (kbd "C-u") 'evil-scroll-up)
+(define-key evil-insert-state-map (kbd "C-u")
+  (lambda ()
+    (interactive)
+    (evil-delete (point-at-bol) (point))))
+
 (require 'evil)
 (evil-mode 1)
 (evil-commentary-mode)
@@ -47,7 +67,15 @@
  (find-file "~/.config/emacs/init.el")
   )
 
-(key-chord-define evil-normal-state-map  "ev"  'get-my-init)
+(defun get-my-shell()
+  "opens init.el in vertical split"
+  (interactive)
+  (split-window-horizontally)
+ (vterm "/bin/bash")
+  )
+
+
+;; (Key-chord-define evil-normal-state-map  "ev"  'get-my-init)
 
 
 ;; auctex options
@@ -62,8 +90,23 @@
 
 
 ;; org mode
-(setq-default org-confirm-babel-evaluate nil)
+(add-hook 'org-mode-hook  (lambda()
+	(setq-default org-confirm-babel-evaluate nil)
+          (org-bullets-mode t)
+	  (require 'org-tempo)))
 
+;; Buffer in same window
+(global-set-key "\C-x\C-b" 'ivy-switch-buffer)
 
-;; buffer in same window
-(global-set-key "\C-x\C-b" 'buffer-menu)
+;; set leader key in all states
+(evil-set-leader 'normal (kbd "SPC"))
+
+(evil-define-key 'normal 'global (kbd "<leader>em")  'get-my-init)
+(evil-define-key 'normal 'global (kbd "<leader>et")  'get-my-shell)
+(global-set-key (kbd "C-x C-SPC") 'ivy-mode)
+
+(add-hook 'after-init-hook 'global-company-mode)
+;; (use-package company
+;;   :config
+  ;; (add-to-list 'company-backends '(company-org-block)))
+
